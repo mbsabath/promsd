c24eff66c599efe332249949545d9154edc0d74c
Ben <bsabath1@gmail.com>
2023-01-10 15:24:00 -0500

    add command line args

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/mbsabath/promsd/api"
 )
 
+var help bool
+var hostname string
+var port string
+
 func main() {
+	flag.BoolVar(&help, "help", false, "Print Usage")
+	flag.StringVar(&hostname, "host", "localhost", "Host to listen on (usually localhost or 0.0.0.0)")
+	flag.StringVar(&port, "port", "5000", "Port to listen on")
+
+	flag.Parse()
+
+	if help {
+		flag.Usage()
+		os.Exit(0)
+	}
+
 	handler := api.NewSdHandler()
-	err := http.ListenAndServe("localhost:6969", handler)
+	err := http.ListenAndServe(fmt.Sprintf("%s:%s", hostname, port), handler)
 	log.Fatal(err)
 }
